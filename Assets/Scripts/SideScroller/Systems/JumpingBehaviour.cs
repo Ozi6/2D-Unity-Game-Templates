4f60c84f6aed1b923092511f10f899c29c840fc5
Ozan <ozankutlar@protonmail.com>
2025-09-18 16:22:00 +0300

    Fix jumping after ground leave

--- Assets/Scripts/SideScroller/Systems/JumpingBehaviour.cs
+++ Assets/Scripts/SideScroller/Systems/JumpingBehaviour.cs
@@ -25,31 +25,57 @@ public class JumpingBehaviour : MonoBehaviour
         HandleJumping();
     }
 
-    private void HandleJumping()
-    {
-        if (Input.GetButtonDown("Jump"))
-        {
-            if (wallCling != null && wallCling.IsWallClinging)
-            {
-                wallCling.PerformWallJump();
-            }
-            else if (core.IsGrounded && !isJumping)
-            {
-                core.Rb.linearVelocity = new Vector2(core.Rb.linearVelocity.x, JumpForce);
-                isJumping = true;
-                jumpHoldTimer = 0f;
-            }
-        }
+    private const float GroundLeaveThreshold = 0.2f;
+	private float groundLeaveTime = float.NegativeInfinity;
 
-        if (Input.GetButton("Jump") && isJumping && jumpHoldTimer < MaxJumpHoldTime && (wallCling == null || !wallCling.IsWallClinging))
-        {
-            core.Rb.AddForce(Vector2.up * JumpForce * JumpHoldMultiplier * Time.deltaTime * 50f);
-            jumpHoldTimer += Time.deltaTime;
-        }
-        else if (Input.GetButtonUp("Jump"))
-            isJumping = false;
+	private void HandleJumping()
+	{
+		// Update ground leave time tracking
+		if (core.IsGrounded)
+		{
+			groundLeaveTime = float.NegativeInfinity;
+		}
+		else
+		{
+			if (groundLeaveTime == float.NegativeInfinity)
+			{
+				groundLeaveTime = Time.time;
+			}
+		}
 
-        if (core.Rb.linearVelocity.y < 0)
-            isJumping = false;
-    }
+		if (Input.GetButtonDown("Jump"))
+		{
+			if (wallCling != null && wallCling.IsWallClinging)
+			{
+				wallCling.PerformWallJump();
+			}
+			else if (core.IsGrounded && !isJumping)
+			{
+				core.Rb.linearVelocity = new Vector2(core.Rb.linearVelocity.x, JumpForce);
+				isJumping = true;
+				jumpHoldTimer = 0f;
+			}
+			else if (!isJumping && groundLeaveTime != float.NegativeInfinity && Time.time - groundLeaveTime < GroundLeaveThreshold)
+			{
+				core.Rb.linearVelocity = new Vector2(core.Rb.linearVelocity.x, JumpForce);
+				isJumping = true;
+				jumpHoldTimer = 0f;
+			}
+		}
+
+		if (Input.GetButton("Jump") && isJumping && jumpHoldTimer < MaxJumpHoldTime && (wallCling == null || !wallCling.IsWallClinging))
+		{
+			core.Rb.AddForce(Vector2.up * JumpForce * JumpHoldMultiplier * Time.deltaTime);
+			jumpHoldTimer += Time.deltaTime;
+		}
+		else if (Input.GetButtonUp("Jump"))
+		{
+			isJumping = false;
+		}
+
+		if (core.Rb.linearVelocity.y < 0)
+		{
+			isJumping = false;
+		}
+	}
 }
